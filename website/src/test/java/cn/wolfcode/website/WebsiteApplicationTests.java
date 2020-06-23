8d20c9ad42b2e2ed3e34d23e95783901e8c9c6c6
chen <chen@wolfcode.cn>
2020-06-23 23:16:25 +0800

    init

---
+++ website/src/test/java/cn/wolfcode/website/WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.wolfcode.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
