54b6d965023f1be94305ec2af6e39d0985b852fb
chenjianfeng <chenjianfeng@163.cn>
2020-06-24 16:20:04 +0800

    添加: springboot对jsp的支持

---
+++ website/src/main/java/cn/wolfcode/website/bean/DogBean.java
@@ -0,0 +1,12 @@
+package cn.wolfcode.website.bean;
+
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor@NoArgsConstructor
+@ToString
+public class DogBean {
+    private Long dogId;
+    private String dogName;
+}
