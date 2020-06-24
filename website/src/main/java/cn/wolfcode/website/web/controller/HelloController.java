f3950aff089e836db8b62baa83d97092b27774f8
chenjianfeng <chenjianfeng@163.cn>
2020-06-24 17:22:34 +0800

    添加: springboot 热部署

--- website/src/main/java/cn/wolfcode/website/web/controller/HelloController.java
+++ website/src/main/java/cn/wolfcode/website/web/controller/HelloController.java
@@ -1,13 +1,24 @@
 package cn.wolfcode.website.web.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+    @Value("${test.hot.deploy}")
+    private String restart;
 
     @RequestMapping("hello")
     public String hello() {
         return "hello";
+    }
+
+    @RequestMapping("restart")
+    @ResponseBody
+    public String restart() {
+        System.out.println("热部署2");
+        return restart;
     }
 }
