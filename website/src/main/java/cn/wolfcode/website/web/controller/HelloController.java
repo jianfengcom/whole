9d818b25108e8954d39e6522623f4f2cb9089621
chenjianfeng <chenjianfeng@163.cn>
2020-07-04 15:26:08 +0800

    请求转发 重定向 jsp html

--- website/src/main/java/cn/wolfcode/website/web/controller/HelloController.java
+++ website/src/main/java/cn/wolfcode/website/web/controller/HelloController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
 public class HelloController {
     @Value("${test.hot.deploy}")
@@ -12,7 +15,24 @@ public class HelloController {
 
     @RequestMapping("hello")
     public String hello() {
-        return "hello";
+//        return "hello";
+        return "redirect:rd";
+    }
+
+    // forward: 该关键字表示不再经过视图解析器
+    @RequestMapping("forwardView")
+    public String forwardView() {
+        return "forward:/WEB-INF/jsp/forwardView.jsp";
+    }
+
+    @RequestMapping("request")
+    public void request(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        request.getRequestDispatcher("/WEB-INF/jsp/forwardView.jsp").forward(request, response);
+    }
+
+    @RequestMapping("rd")
+    public String rd() {
+        return "redirect";
     }
 
     @RequestMapping("restart")
@@ -20,5 +40,12 @@ public class HelloController {
     public String restart() {
         System.out.println("热部署0");
         return restart;
+    }
+
+    public static void main(String[] args) { // 1593758180147 1593758215945
+        System.out.println(System.currentTimeMillis()/1000);
+//        System.out.println(1593758215945L / 1000);
+        /*System.out.println(1593758215945L - 1593758180147L);
+        System.out.println(35798 * 2 * 5);*/
     }
 }
---
+++ website/src/main/webapp/WEB-INF/jsp/forwardView.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: jianf
+  Date: 2020/7/4
+  Time: 14:17
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>/WEB-INF/forwardView.jsp
+
+</body>
+</html>
