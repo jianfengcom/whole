5005340ee18f28dff66dc9903822963e62df04fa
chenjianfeng <chenjianfeng@163.cn>
2020-10-21 17:58:55 +0800

    website - update

---
+++ lib_rank.sql
@@ -0,0 +1,152 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50525
+ Source Host           : 127.0.0.1:3306
+ Source Schema         : ibernate
+
+ Target Server Type    : MySQL
+ Target Server Version : 50525
+ File Encoding         : 65001
+
+ Date: 21/10/2020 17:57:51
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for lib_rank
+-- ----------------------------
+DROP TABLE IF EXISTS `lib_rank`;
+CREATE TABLE `lib_rank`  (
+  `rank_id` int(11) NOT NULL,
+  `category_id` int(11) NULL DEFAULT NULL,
+  `designer_id` int(11) NULL DEFAULT NULL,
+  `brand_minfo` varchar(255) CHARACTER SET gbk COLLATE gbk_chinese_ci NULL DEFAULT NULL COMMENT '产品主推信息',
+  `product_minfo` varchar(255) CHARACTER SET gbk COLLATE gbk_chinese_ci NULL DEFAULT NULL COMMENT '品牌主推信息',
+  PRIMARY KEY (`rank_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = gbk COLLATE = gbk_chinese_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of lib_rank
+-- ----------------------------
+INSERT INTO `lib_rank` VALUES (18, 414, 2964, '福州择木创建室内设计总监。在设计风格上以现代简约、美式、北欧、日式见长，敢于突破、创新，尝试新手法。设计作品风格各异，设计手法大气、简炼，同时注重细节的精致，对空间有很强的把握能力。', '福州择木创建室内设计总监。在设计风格上以现代简约、美式、北欧、日式见长，敢于突破、创新，尝试新手法。设计作品风格各异，设计手法大气、简炼，同时注重细节的精致，对空间有很强的把握能力。');
+INSERT INTO `lib_rank` VALUES (19, 607, 15913, '陈朝辉设计工作室设计总监，毕业于北京师范大学&湛江艺术学校，曾获得各类空间设计奖项40多个。部分荣誉：2019年中国住宅设计精英榜-广东100强。2018年度中国软装设计大赛「龙承奖」商业空间优秀奖。2018年香港室内设计师协会亚太设计师大赛工程类办公室空间钻石奖荣。', '陈朝辉设计工作室设计总监，毕业于北京师范大学&湛江艺术学校，曾获得各类空间设计奖项40多个。部分荣誉：2019年中国住宅设计精英榜-广东100强。2018年度中国软装设计大赛「龙承奖」商业空间优秀奖。2018年香港室内设计师协会亚太设计师大赛工程类办公室空间钻石奖荣。');
+INSERT INTO `lib_rank` VALUES (20, 662, 72122, '沈阳东易日盛装饰设计主案设计师，从业经验6年，擅长家装设计，家居装饰搭配。', '沈阳东易日盛装饰设计主案设计师，从业经验6年，擅长家装设计，家居装饰搭配。');
+INSERT INTO `lib_rank` VALUES (21, 317, 14611, '网络知名设计师，清羽室内装饰设计有限公司设计总监，中央电视台特约设计师，从事室内设计行业超15年，倡导以“环保+功能+效果”的平衡设计理念，创造个性舒适的居住空间。', '网络知名设计师，清羽室内装饰设计有限公司设计总监，中央电视台特约设计师，从事室内设计行业超15年，倡导以“环保+功能+效果”的平衡设计理念，创造个性舒适的居住空间。');
+INSERT INTO `lib_rank` VALUES (22, 653, 13124, '圣奇凯尚装饰联合创始人/设计总监，装饰协会注册设计师：高级设计师。擅长设计：欧美、新古典、后现代简约工业风、混搭风格。做有温度的设计，设计不拘泥一格、经纬有度，以人与自然环境和谐为根本，人所居，基长青。', '圣奇凯尚装饰联合创始人/设计总监，装饰协会注册设计师：高级设计师。擅长设计：欧美、新古典、后现代简约工业风、混搭风格。做有温度的设计，设计不拘泥一格、经纬有度，以人与自然环境和谐为根本，人所居，基长青。');
+INSERT INTO `lib_rank` VALUES (23, 256, 15748, '深圳见微陈设艺术设计有限公司创始人兼设计总监，从事家装纯设计十余年，作品多次登录CCTV2<榜样空间》榜样作品，中国美院进修生。', '深圳见微陈设艺术设计有限公司创始人兼设计总监，从事家装纯设计十余年，作品多次登录CCTV2<榜样空间》榜样作品，中国美院进修生。');
+
+-- ----------------------------
+-- Table structure for lib_rank_brand
+-- ----------------------------
+DROP TABLE IF EXISTS `lib_rank_brand`;
+CREATE TABLE `lib_rank_brand`  (
+  `rank_id` int(11) NOT NULL COMMENT '榜单id',
+  `brand_id` int(11) NOT NULL COMMENT '品牌id',
+  `reason` varchar(255) CHARACTER SET gbk COLLATE gbk_chinese_ci NULL DEFAULT NULL COMMENT '推荐理由',
+  `pv_month` int(11) NULL DEFAULT NULL COMMENT '月PV流量',
+  `hot_value` int(255) NULL DEFAULT NULL COMMENT '热度',
+  `is_hand_set` tinyint(2) NULL DEFAULT NULL COMMENT '是否手动设置',
+  `status` tinyint(2) NULL DEFAULT NULL COMMENT '状态',
+  `create_by` varchar(50) CHARACTER SET gbk COLLATE gbk_chinese_ci NULL DEFAULT NULL,
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建日期',
+  `update_by` varchar(50) CHARACTER SET gbk COLLATE gbk_chinese_ci NULL DEFAULT NULL,
+  `update_time` datetime NULL DEFAULT NULL COMMENT '更新日期',
+  PRIMARY KEY (`rank_id`, `brand_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = gbk COLLATE = gbk_chinese_ci COMMENT = '产品榜单' ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for lib_rank_product
+-- ----------------------------
+DROP TABLE IF EXISTS `lib_rank_product`;
+CREATE TABLE `lib_rank_product`  (
+  `rank_id` int(11) NOT NULL COMMENT '榜单id',
+  `product_id` int(11) NOT NULL COMMENT '产品id',
+  `reason` varchar(255) CHARACTER SET gbk COLLATE gbk_chinese_ci NULL DEFAULT NULL COMMENT '推荐理由',
+  `pv_month` int(11) NULL DEFAULT NULL COMMENT '月PV流量',
+  `hot_value` int(255) NULL DEFAULT NULL COMMENT '热度',
+  `is_hand_set` tinyint(2) NULL DEFAULT NULL COMMENT '是否手动设置',
+  `status` tinyint(2) NULL DEFAULT NULL COMMENT '状态',
+  `create_by` varchar(50) CHARACTER SET gbk COLLATE gbk_chinese_ci NULL DEFAULT NULL,
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建日期',
+  `update_by` varchar(50) CHARACTER SET gbk COLLATE gbk_chinese_ci NULL DEFAULT NULL,
+  `update_time` datetime NULL DEFAULT NULL COMMENT '更新日期',
+  PRIMARY KEY (`rank_id`, `product_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = gbk COLLATE = gbk_chinese_ci COMMENT = '产品榜单' ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of lib_rank_product
+-- ----------------------------
+INSERT INTO `lib_rank_product` VALUES (18, 725972, '这款潜水艇地漏不锈钢主体的加厚面板，达10mm，锻压一体成型无焊接，耐用耐腐蚀，表面拉丝哑光处理，耐磨不生锈。', NULL, 100, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (18, 725973, '加宽加大镂空面积，排水速度更快更稳定，利用重力及机械原理实现密封功能，下水接头配备硅胶密封圈紧固，双重密封防臭防虫。', NULL, 99, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (18, 725974, '使用一体锻造黄铜材质和多层电镀工艺，哑黑质感，高而不冷，浴室多一份轻奢之感。地漏芯采用ABS材质，运用重力原理和磁吸自闭技术密封闭合，有效防止污水返溢。', NULL, 98, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (18, 725975, '排水盖板、过滤网、内芯均为不锈钢材质，耐腐蚀抗生锈，双重过滤防堵塞，面板作钝角打磨处理，防刮伤，滤芯采用反向重力原理闭合设计，防止异味。', NULL, 97, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (18, 725976, '50%镂空排水面积，排水更快，双重过滤发丝等杂质，告别下水道堵塞，反向重力闭合设计，防止异味进入室内。', NULL, 96, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (18, 725977, '采用精选黄铜锻造，多层电镀表面，加厚面板更耐用，多道工序精磨钝角，表面无毛刺，防刮不伤脚，精铜芯采用回止弹簧，达到有效密封效果。', NULL, 95, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (18, 725978, '采用304不锈钢锻造面板，不锈钢拉丝加上精雕钝角，耐用耐磨，不伤脚，双重滤网有效拦截毛发等杂物，可拆卸清洗，方便日常清洁。', NULL, 94, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (18, 725979, '精致镂空格纹打造地漏面板，更大限度设计格与格合理间距，时尚美观不失实用性，加大加宽的镂空面积让排水更大更快，甄选优质铜合金锻造，耐磨耐腐。', NULL, 93, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (19, 726004, '这款产品采用喷杆304不锈钢材质，一体设计，表明光滑，不留污垢；并且喷嘴设计上十分精心，每次使用前后会自动全方位持续清晰，让消费者拥有更健康的美好生活。', NULL, 100, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (19, 726005, '这款产品最大的亮点就是能随时随地想用就用。冲刷用水采用主动供水模式，不受外界自来水管道水压波动影响，冲力更大。高楼层在用水高峰期也可以放心使用，', NULL, 99, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (19, 726006, '这款产品是即热恒温供水，采用日本进口陶瓷加热芯，冷水一入热水即出。无论何时开启，水温都是恰到好处，让人倍感舒适。', NULL, 98, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (19, 726007, '这款智能马桶在设计上十分贴心，一旋一按，轻松重新，便捷操作方便全家人使用，能够自在享受智能如厕体验。', NULL, 97, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (19, 726008, '采用创新双重洁净技术，一次启动两次虹吸，双重洁净。连续两次排空马桶内的污垢真正做到无残留冲刷，能满足国标二级水效的要求。', NULL, 96, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (19, 726009, '主打亮点是大冲力，重新定义冲水方式。三次迭代造就全新飓风助力冲水系统，低水压要求，外形时尚美观，既是实力派又是偶像派。', NULL, 95, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (19, 726010, '这款产品十分专业，注重过滤系统产品研发，去除水中杂质，过滤所含重金属颗粒等杂质，让活水更干净，放心洗PP。', NULL, 94, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (19, 726011, '产品设计上十分贴心，4档智能调温让座圈加热更加舒适。活水注入，一秒即热，能够快速清洁。洗后暖风烘干，能随心控制风温。', NULL, 93, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (20, 725980, '这款烤箱拥有38L大容量，可轻松装下10人大餐，三种加热模式设计，满足各种食材的烘烤温度，烤出不同温度，广域调控，精准控温。', NULL, 100, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (20, 725981, '这款烤箱拥有40L大容量，加深设计，四旋钮独立控温，食材上色更加均匀，实用钻石内胆设计，更加满足多层加热均匀覆盖，是居家不错的选择。', NULL, 99, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (20, 725982, '35L独立控温大烤箱适合家庭使用，具有三层拷位，使用灵活方便，还具备120分钟的长通定时器设计，享受开趴的激情，厨房也能花式烘焙。', NULL, 98, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (20, 725983, '格兰仕K5E专业电烤箱具备32L大容量，升级31mm加厚玻璃门板，隔热防烫，腔内温度更加稳定，助你烘焙成功，上下独立控温，美食口感随你掌控。', NULL, 97, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (20, 725984, '美的PT3502烤箱上下管独立控温，创新隔热聚能面板设计，玩出烘焙新花样，满足多种烘焙需求，4管平衡加热平均加热，锁住色香味，以美食致美好。', NULL, 96, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (20, 725985, '海氏C40升级款具备40L超高颜值，电子式精准控温，把控更精准，使用搪瓷不粘内胆，健康耐用易清洁，无论哪种幕后玩家，都能玩出不同的花样。', NULL, 95, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (20, 725986, '这款烤箱具备均衡瓷窑烤，四星级的出色烘焙，意大利进口搪瓷原材能将食材烘焙的更加到位，性能稳定，锁定效果最好，上下管独立控温，能够让烘焙新手实现资深达人的出色表现。', NULL, 94, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (20, 725987, '这款烤箱拥有30L大容量，上下管独立控温，欧式外观的出众设计，优雅的诠释轻奢时尚，上下管独立控温，火候随时掌控，平衡式的发热管，左右温场均衡，美食烤出好气色。', NULL, 93, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (21, 725988, '这款美的麒麟净水器具备600G的大容量，双出水设计，只为一杯鲜纯好水，从源头呵护全家健康饮水，制水速度出众，尽情享用无须等待。', NULL, 100, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (21, 725989, '这款九阳超滤净水器具备多层过滤设备，深层净化矿物质水，304不锈钢机身，坚固耐用不生锈，密封性强，避免水质二次污染，呵护家庭健康饮水。', NULL, 99, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (21, 725990, '这款是小米净水器厨下式的设计，大流量直出纯净水，健康家庭必备，可以隐藏安装在厨房水槽下方的橱柜中，既不影响装修风格，也节省厨房有限的台面空间。', NULL, 98, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (21, 725991, '这款沁园净水器设计了双出水，五级高精过滤，智能一键冲洗，600G大通量，做到真正的双模双吹水，双水质设计，满足家庭全家饮用水需求。', NULL, 97, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (21, 725992, '这款净水器具备大流量设计，加粗滤芯，出水快又稳，打开龙头就能喝，矿物质水即滤即饮，过滤面积大，耐污能力强，提高滤芯使用寿命。', NULL, 96, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (21, 725993, '这款苏泊尔净水器具备五级的精过滤，保留了矿物质，深层净化水质，优质高分子超滤膜滤芯，能有效滤除家中自来水的杂质和细菌，去除异味，改善口感，安全饮水。', NULL, 95, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (21, 725994, '这款净水器拥有600加仑直出大流量，畅快饮水，智能灯环水龙头设计，可自主换芯，一按一拧，自己可轻松更换，采用大通量反渗透过滤技术，无存水罐，拒绝二次污染。', NULL, 94, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (21, 725995, '德克西五级精滤净水器，二代升级滤芯设计，时尚的外观，物理过滤，无须用电，不产生废水，零能耗天然过滤，台下式隐藏安装设计，不占用空间。', NULL, 93, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (22, 725996, '这款南极人四件套才用全棉面料，亲肤透气，制作工艺环保，无损人体健康，浅绿白条纹格子设计，将自然清新带进卧室。', NULL, 100, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (22, 725997, '精选好棉配上高支高密精编工艺，亲肤舒适，妙不可言的双版设计，感受轻奢品味，全面面料搭配涂料印染，不易退色，缩水率更低。', NULL, 99, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (22, 725998, '100%全棉制作，柔软舒适，干爽透气，给你一个健康睡眠，对高质量的追求体现在每个细节中，拉链顺畅不卡链。', NULL, 98, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (22, 725999, '水星家纺这款清新花卉系四件套已花卉为设计灵感，将缤纷花卉与色彩融入到空间中，面料为精选全棉，更柔软舒适，采用环保印染工艺，花型层次感丰富，久洗不褪色。', NULL, 97, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (22, 726000, '用简约条纹搭配素净蓝调，让居家风格坐拥沉稳品格，打破老派英伦风的刻板，采用专业的印染工艺，不易对皮肤产生刺激，色牢度高。', NULL, 96, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (22, 726001, '这款春秋被采用中空螺旋纤维填充，透气透湿，手感蓬松柔软，保暖性极佳，表面采用提花工艺，简介雅致，被感柔滑有光泽。', NULL, 95, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (22, 726002, '40S斜纹精纺细纱，透气透湿，锁住温润干爽，AB版双面设计，面料紧实，亲肤细腻，有床单和床笠款可选，满足铺床的不同需求。', NULL, 94, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (22, 726003, '罗莱家纺这款四件套已现代风格为基准，时尚大方，适合多种家居风格，采用新疆优质棉花和斜纹织造手法，透气厚实，活性印染工艺环保耐用，色牢度高，色彩亮丽。', NULL, 93, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (23, 726012, '这块产品采用灵感感应技术，拥有出众指标，能够快速长效除甲醛。纳米级滤网，三步主动除甲醛，一键操作面板，能够触屏实时操作。', NULL, 100, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (23, 726013, '这块产品拥有智能化设计，采用智能双重感应器，实时反映空气质量。以强风模式下，快速运转的风扇可迅速提供健康空气。', NULL, 99, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (23, 726014, '这款产品设计十分精心，三面进风，卓越能力，能快速净化室内过敏原病毒，时刻守护家人的健康。红外传感器搭配“光动眼”灵敏感应技术，可精准检测空气质量，净化更彻底。', NULL, 98, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (23, 726015, '这个产品过滤效率高效持久，四层八面渔网粘碳结构，分布均匀，层层过滤。吸附能力更强，甲醛分子与微孔更匹配，添加Air-Reborn除醛配方定向吸附甲醛能力更强。', NULL, 97, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (23, 726016, '拥有高效HEPA滤网，过滤精度0.3μm，快速净化PM2.5/TVOC有害气体/过敏原/异味空气净化高效，一次过滤效率可达99.5%。', NULL, 96, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (23, 726017, '产品采用纳米二氧化硅，纳米级的均匀微孔结构，专门针对甲醛等小分子有害污染有良好的的吸附作用。并使用新型静电驻极滤材，拦截集尘更出色。', NULL, 95, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (23, 726018, '采用表面积800-1000㎡/g配方椰壳微孔活性炭，快速净化板材、木地板等释放装修甲醛。搭载灵敏颗粒物传感器，自动急速除烟，呵护全家人的健康生活。', NULL, 94, 1, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `lib_rank_product` VALUES (23, 726019, '产品采用360°大滤网设计，空气快速对流，性能表现出众。强劲去除小微米颗粒物，快速净化PM2.5/过敏源/有害气体、滤网展开面积高达4㎡，使用时间更长，能够深度吸附甲醛、甲苯。', NULL, 93, 1, NULL, NULL, NULL, NULL, NULL);
+
+-- ----------------------------
+-- Table structure for lib_rank_product_show
+-- ----------------------------
+DROP TABLE IF EXISTS `lib_rank_product_show`;
+CREATE TABLE `lib_rank_product_show`  (
+  `rank_id` int(11) NOT NULL AUTO_INCREMENT,
+  `sort` int(11) NULL DEFAULT 99 COMMENT '排序',
+  `create_by` varchar(50) CHARACTER SET gbk COLLATE gbk_chinese_ci NULL DEFAULT NULL,
+  `create_time` datetime NULL DEFAULT NULL COMMENT '创建日期',
+  `update_by` varchar(50) CHARACTER SET gbk COLLATE gbk_chinese_ci NULL DEFAULT NULL,
+  `update_time` datetime NULL DEFAULT NULL COMMENT '更新日期',
+  PRIMARY KEY (`rank_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 19 CHARACTER SET = gbk COLLATE = gbk_chinese_ci COMMENT = '产品榜单展示' ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of lib_rank_product_show
+-- ----------------------------
+INSERT INTO `lib_rank_product_show` VALUES (18, 18, 'admin', '2020-10-21 17:57:19', 'admin', '2020-10-21 17:57:26');
+
+SET FOREIGN_KEY_CHECKS = 1;
